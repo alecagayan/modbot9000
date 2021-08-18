5c21e02145916a616e5f895300d15ad9acff94c1
alec <alecagayan24@gmail.com>
2021-08-18 11:59:08 -0400

    gitignore and serverlist

--- bot.py
+++ bot.py
@@ -13,21 +13,19 @@ client.load_extension("cogs.dm")
 client.load_extension("cogs.kickban")
 client.load_extension("cogs.welcomer")
 client.load_extension("cogs.cooldown")
-
-
+client.load_extension("cogs.misc")
 
 
 @client.event
 async def on_ready():
 
     print("Bot online!\n")
-#    print("Discord.py API version:", discord.__version__)
-#    print("Python version:", platform.python_version())
-#    print("Running on:", platform.system(), platform.release(), "(" + os.name + ")")
-#    print("Name : {}".format(client.user.name))
-#    print("Client ID : {}".format(client.user.id))
-#    print("Currently active on " + str(len(client.guilds)) + " server(s).\n")
-#    logger.info("Bot started successfully.")
+    print("Discord.py API version:", discord.__version__)
+    print("Python version:", platform.python_version())
+    print("Running on:", platform.system(), platform.release(), "(" + os.name + ")")
+    print("Name : {}".format(client.user.name))
+    print("Client ID : {}".format(client.user.id))
+    print("Currently active on " + str(len(client.guilds)) + " server(s).\n")
 
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="over everyone"))
 
---
+++ cogs/misc.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import json
+
+class Misc(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def serverlist(self, ctx):
+        """List the servers that the bot is active on."""
+        x = ', '.join([str(server) for server in self.bot.guilds])
+        y = len(self.bot.guilds)
+        print("Server list: " + x)
+        if y > 40:
+            embed = discord.Embed(title="Currently active on " + str(y) + " servers:", description="```json\nCan't display more than 40 servers!```", colour=0xFFFFF)
+            return await ctx.send(embed=embed)
+        elif y < 40:
+            embed = discord.Embed(title="Currently active on " + str(y) + " servers:", description="```json\n" + x + "```", colour=0xFFFFF)
+            return await ctx.send(embed=embed)
+            
+def setup(bot):
+    bot.add_cog(Misc(bot))
