1f0da462acc9cfe72e905fd41775e819e5bd5f67
alec <alecagayan24@gmail.com>
2021-08-19 23:36:28 -0400

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import json
+import random
 
 class Misc(commands.Cog):
     def __init__(self, bot):
@@ -18,6 +19,15 @@ class Misc(commands.Cog):
         elif y < 40:
             embed = discord.Embed(title="Currently active on " + str(y) + " servers:", description="```json\n" + x + "```", colour=0xFFD414)
             return await ctx.send(embed=embed)
+
+    @commands.command()
+    async def ping(self, ctx):
+        """
+        Pings the bot.
+        """
+        joke = random.choice(["NO FEAR", "MO INTERNET BABEH", "fire it up", "LIGHTNING"])
+        ping_msg = await ctx.send("Pinging Server...")
+        await ping_msg.edit(content=joke + f" // ***{self.bot.latency*1000:.0f}ms***")
 
 def setup(bot):
     bot.add_cog(Misc(bot))
